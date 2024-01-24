827be2f0675fe0bfec28665e19d444aa4a3c586e
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-01-24 07:08:36 -0300

    Finalização do programa hello.asm

--- aula01/hello.asm
+++ aula01/hello.asm
@@ -1,12 +1,23 @@
-// nasm -f elf64 hello.asm 
+; Compilar - Transformar um programa em linguagem de máquina
+    ; nasm -f elf64 hello.asm 
+; Linkeditar - Transformar um programa em linguagem de máquina em um executável
+    ; ld -s -o nomedoarquivo nomedoarquivo.o
 
 section .data
+    msg db 'Hello World!', 0xA
+    tam equ $- msg
 
 section .text
 
 global _start
 
 _start:
+    mov eax, 0x4
+    mov ebx, 0x1
+    mov ecx, msg
+    mov edx, tam
+    int 0x80
+
     mov eax, 0x1 
     mov ebx, 0x0
     int 0x80
