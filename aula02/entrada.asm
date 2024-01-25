cd19563e94aa3aaaa4566f59f9bf2c86997e0c28
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-01-24 21:16:12 -0300

    Desenvolvimento de programa de entrada de dados

---
+++ aula02/entrada.asm
@@ -0,0 +1,43 @@
+segment .data
+    LF equ 0xA ; Line Feed
+    NULL equ 0xD ; Final da cadeia de caracteres
+    SYS_CALL equ 0x80 ; Envia informações ao Sistema Operacional
+
+    ;EAX
+    SYS_EXIT equ 0x1 ; Código de chamada para finalizar
+    SYS_READ equ 0x3 ; Operação de Leitura
+    SYS_WRITE equ 0x4 ; Operação de escrita
+
+    ;EBX
+    RET_EXIT equ 0x0 ; Operação Realizada com Sucesso
+    STD_IN equ 0x0 ; Entrada padrão
+    STD_OUT equ 0x1 ; Saída padrão
+
+section .data
+    msg db 'Entre com seu nome:', LF, NULL
+    tam equ $- msg
+
+section .bss
+    nome resb 1
+
+section .text
+
+global _start
+
+_start:
+    mov EAX, SYS_WRITE
+    mov EBX, STD_OUT
+    mov ECX, msg
+    mov EDX, tam
+    int SYS_CALL
+
+    mov EAX, SYS_READ
+    mov EBX, STD_IN
+    mov ECX, nome
+    mov EDX, 0xA
+    int SYS_CALL
+
+    mov EAX, SYS_EXIT
+    mov EBX, RET_EXIT
+    int SYS_CALL
+
